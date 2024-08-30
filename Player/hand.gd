e1e4b2a6d5e26f5c52e8e31ce0928c5f092d921f
pmro1 <p.mroz.zywiol@gmail.com>
2024-08-30 18:35:57 +0200

    feat: Update player's hand position and add gun_0001 model

---
+++ Player/hand.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+const ADS_LERP = 20.0
+const FOV_TOLERANCE = 5
+
+@export var camera_path: NodePath
+@export var default_position: Vector3
+@export var ads_position: Vector3
--- Player/player.gd
+++ Player/player.gd
@@ -1,6 +1,5 @@
 extends CharacterBody3D
 
-
 var speed
 const WALK_SPEED =5.0 
 const SPRINT_SPEED = 8.0
