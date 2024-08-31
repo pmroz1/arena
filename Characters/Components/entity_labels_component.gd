1e6c48be64a80da13e8cb072d520083f34a7176f
pmro1 <p.mroz.zywiol@gmail.com>
2024-08-31 13:08:18 +0200

    refactor: Update EntityLabelsComponent to use attributes for label updates
    This commit refactors the EntityLabelsComponent to use the attributes from the AttributesComponent for updating the labels. The set_entity_name and set_level methods are removed, and a new update_labels method is added to update the name and level labels based on the attributes. The getCharacterTypeColor method is also added to determine the color of the labels based on the entityType attribute. This change improves code organization and reduces redundancy in label updates.

--- Characters/Components/entity_labels_component.gd
+++ Characters/Components/entity_labels_component.gd
@@ -6,34 +6,56 @@ class_name EntityLabelsComponent
 # Exported references to Label3D nodes
 @onready var name_label: Label3D = $Name
 @onready var level_label: Label3D =$Name/Level
+@onready var attributes = $"../AttributesComponent"
 
-@export var entity: Node3D
+@export var entityBody: Node3D
 @export var height_offset : float = 2.0
-
-# Methods to update the labels
-func set_entity_name(entityName: String) -> void:
-	print("Setting entity name to: " + entityName)
-	if name_label:
-		name_label.text = entityName
-
-func set_level(level: int) -> void:
-	if level_label:
-		level_label.text = "Level: " + str(level)
 
 func move_labels_above_entity(heightOverride: int = -1) -> void:
 	# if heightOverride is not null, use it as the height offset
 	if heightOverride != -1:
 		height_offset = heightOverride
 
-	if entity:
-		var entity_global_position = entity.global_transform.origin
+	if entityBody:
+		var entity_global_position = entityBody.global_transform.origin
 		var label_position = entity_global_position + Vector3(0, height_offset, 0)
 		name_label.global_transform.origin = label_position
+
+
+func getCharacterTypeColor():
+	# player, npc, enemy, boss, special
+	match attributes.entityType:
+		"player":
+			return Color(0, 1, 0)
+		"npc":
+			return Color(0, 0, 1)
+		"enemy":
+			return Color(1, 0, 0)
+		"boss":
+			return Color(1, 0, 1)
+		"special":
+			return Color(1, 1, 0)
+		_:
+			return Color(1, 1, 1)
+	
+func change_labels_color(color: Color):
+	print("color", color)
+	name_label.modulate = color
+	level_label.modulate = color
+
+func update_labels():
+	name_label.text = attributes.entityName
+	level_label.text = str(attributes.level)
+	change_labels_color(getCharacterTypeColor())
+	pass
+
+func setup_defaults():
+	move_labels_above_entity()
+	update_labels()
+	pass
 
 # Initialize with default values
 func _ready():
 	print("EntityLabelsComponent ready.")
-	#set_entity_name("Unnamed")
-	#set_level(1)
-	#move_labels_above_entity()
+	setup_defaults()
 	pass
--- Characters/NPC/guard.gd
+++ Characters/NPC/guard.gd
@@ -3,20 +3,15 @@ extends Node3D
 
 # References to the components
 @onready var attributes_component: AttributesComponent = $AttributesComponent
-@onready var label_component: EntityLabelsComponent = $EntityLabelsComponent
+@onready var labels_component : EntityLabelsComponent = $EntityLabelsComponent
 
 func _ready():
 	print("Character ready.")
-	# Initialize attributes and labels
-	attributes_component.set_entity_name("Guard")
-	attributes_component.set_level(5)
-	
-	
-
-	# Update the labels to reflect the attributes
-	label_component.set_entity_name(attributes_component.get_entity_name())
-	label_component.set_level(attributes_component.get_level())
-
-	# Simulate attribute changes
-	attributes_component.increase_level()
-	label_component.set_level(attributes_component.get_level())  # Update label to new level
+#
+	## Update the labels to reflect the attributes
+	#label_component.set_entity_name(attributes_component.get_entity_name())
+	#label_component.set_level(attributes_component.get_level())
+#
+	## Simulate attribute changes
+	#attributes_component.increase_level()
+	#label_component.set_level(attributes_component.get_level())  # Update label to new level
